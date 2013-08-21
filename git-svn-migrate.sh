3a18f164cfe3b2dd21509f7d9301951009dddb12
Roman Hoog Antink <rha@open.ch>
2013-08-21 13:52:00 +0200

    allow multiple concurrent processes
    ...by using random temporary directories.

--- git-svn-migrate.sh
+++ git-svn-migrate.sh
@@ -166,7 +166,7 @@ fi
 
 # Process each URL in the repository list.
 pwd=`pwd`;
-tmp_destination="$pwd/tmp-git-repo";
+tmp_destination="`mktemp --directory --dry-run $pwd/tmp-git-repoXXXXXXXX`";
 mkdir -p $destination;
 destination=`cd $destination; pwd`; #Absolute path.
 
